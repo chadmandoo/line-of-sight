6df4401318d6d88b095000f4450359edff3c2fb9
cpeppers <chadpeppers2@gmail.com>
2017-01-17 11:49:59 -0600

    cli fixes

--- cli-config.php
+++ cli-config.php
@@ -1,6 +1,15 @@
 <?php
 
-require_once 'app/autoload.php';
+define("APP_PATH", dirname(__DIR__));
+define("APP_PATH_SRC", dirname(__DIR__).'/src');
+define("APP_PATH_APP", dirname(__DIR__).'/app');
+define("APP_PATH_CONFIG", dirname(__DIR__).'/app/config');
+define("APP_PATH_CACHE", dirname(__DIR__).'/app/cache');
+
+/**
+ * Bootstrap application.
+ */
+require APP_PATH.'/app/autoload.php';
 
 $managerWrapper = new \Los\Core\Entity\EntityManagerWrapper(new \Los\Core\Config\LosConfig());
 
